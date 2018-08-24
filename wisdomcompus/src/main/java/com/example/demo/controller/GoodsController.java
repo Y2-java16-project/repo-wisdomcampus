f586c825799e4245ba9b6ef4c69527f8ec6c874e
pengbeiying <42225734+pengbeiying@users.noreply.github.com>
2018-08-24 10:05:59 +0800

    成功访问到页面

--- wisdomcompus/src/main/java/com/example/demo/controller/GoodsController.java
+++ wisdomcompus/src/main/java/com/example/demo/controller/GoodsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.pojo.Goods;
@@ -33,4 +34,10 @@ public class GoodsController {
 	public String toIndex(Model model) {
 		return "view/index.html";
 	}
+	
+	@PostMapping("/tobbb")
+	public String tobbb() {
+		System.out.println("傻逼的世界");
+		return "{user : 'sb',passwrod : 123456}";
+	} 
 }
